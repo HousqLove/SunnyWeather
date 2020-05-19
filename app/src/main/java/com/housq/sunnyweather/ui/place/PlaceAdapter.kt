5c05b01f468727ebfb26320cf39ae4c7999f7339
housq <housq@robusoft.com.cn>
2020-05-19 10:55:34 +0800

    编码下拉刷新天气，切换城市功能

--- app/src/main/java/com/housq/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/housq/sunnyweather/ui/place/PlaceAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.housq.sunnyweather.R
 import com.housq.sunnyweather.logic.model.Place
 import com.housq.sunnyweather.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.activity_weather.*
 
 class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: List<Place>) :
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
@@ -21,14 +22,24 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
         viewHolder.itemView.setOnClickListener {
             val adapterPosition = viewHolder.adapterPosition
             val place = placeList[adapterPosition]
-            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
-                putExtra("location_lng", place.location.lng)
-                putExtra("location_lat", place.location.lat)
-                putExtra("place_name", place.name)
+            val activity = fragment.activity
+            if (activity is WeatherActivity) {
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            } else {
+
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.location.lng)
+                    putExtra("location_lat", place.location.lat)
+                    putExtra("place_name", place.name)
+                }
+                fragment.startActivity(intent)
+                fragment.activity?.finish()
             }
             fragment.viewModel.savePlace(place)
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
         }
         return viewHolder
     }
